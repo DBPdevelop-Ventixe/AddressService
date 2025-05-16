66ae7921abfe10b39c6a6e8b4166c7f079e15843
Robin <156453367+hbgroca@users.noreply.github.com>
2025-05-16 21:33:46 +0200

    Update

--- AddressWebApi/Data/DataContext.cs
+++ AddressWebApi/Data/DataContext.cs
@@ -6,4 +6,22 @@ namespace AddressWebApi.Data;
 public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
 {
     public DbSet<AddressEntity> Addresses { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<AddressEntity>(entity =>
+        {
+            entity.HasData(new AddressEntity
+            {
+                Id = "1",
+                Street = "123 Sunset St",
+                City = "Miami",
+                ZipCode = "4153",
+                State = "FL",
+                Country = "USA"
+            });
+        });
+    }
 }
--- AddressWebApi/Migrations/20250516095311_InitAddressApiDb.Designer.cs
+++ AddressWebApi/Migrations/20250516095311_InitAddressApiDb.Designer.cs
@@ -3,6 +3,7 @@ using AddressWebApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AddressWebApi.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20250516095311_InitAddressApiDb")]
+    partial class InitAddressApiDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
