f731948768461824a7f0764a8e158210fc58e973
Robin <156453367+hbgroca@users.noreply.github.com>
2025-05-30 12:18:22 +0200

    Added Azure KeyVault for database connection

--- AddressWebApi/Program.cs
+++ AddressWebApi/Program.cs
@@ -1,15 +1,35 @@
 using AddressWebApi.Data;
 using AddressWebApi.Services;
+using Azure.Extensions.AspNetCore.Configuration.Secrets;
+using Azure.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// KeyVault configuration
+var keyVaultUri = new Uri($"https://{builder.Configuration["KeyVaultName"]}.vault.azure.net/");
+
+if (builder.Environment.IsProduction())
+{
+    builder.Configuration.AddAzureKeyVault(
+        keyVaultUri,
+        new DefaultAzureCredential(),
+        //new CustomSecretManager("Ventixe")
+        new AzureKeyVaultConfigurationOptions()
+        {
+            Manager = new CustomSecretManager("Ventixe"),
+            ReloadInterval = TimeSpan.FromMinutes(5)
+        }
+    );
+}
+
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 builder.Services.AddGrpc();
 
 builder.Services.AddDbContext<DataContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("AddressDatabaseConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlDbConnection")));
 
 var app = builder.Build();
 
